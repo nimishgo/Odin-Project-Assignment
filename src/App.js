c5173c44b4f6034ccd40fc4bcb778fb64a6f9e69
nimishgo <nimish.2711@gmail.com>
2022-10-06 15:40:15 +0530

    added methods to handle change and to add task to tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import './App.css';
+import { Component } from 'react';
+import uniqid from 'uniqid';
 
-function App() {
-  return (
-      <div>Hello World</div>
-  );
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid(),
+      },
+      tasks: [],
+    }
+  };
+
+  handleChange = (e) => {
+          this.setState({
+            task: {
+              task: e.target.value,
+              id: this.state.task.id,
+            },
+          }
+        );
+  };
+  
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: '',
+        id: uniqid()
+      },
+    })
+  }
+
+  render() {
+    const { task, tasks } = this.state;
+    
+    return (
+      <div>
+        <form>
+          <label htmlFor="taskInput">Enter the task&nbsp;: </label>
+          <input 
+          type="text" 
+          id='taskInput'
+          onChange={this.handleChange}
+          />
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
